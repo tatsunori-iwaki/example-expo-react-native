2b005057b230ad7c6bd5222dfd4028d37ed4325c
tatsunori-iwaki <iwaki.tatsunori@uk-holdings.co.jp>
2021-06-02 22:24:42 +0900

    change gl-view

--- screen/TreeJsCubeScreen.js
+++ screen/TreeJsCubeScreen.js
@@ -1,78 +1,38 @@
 import * as React from 'react';
 import * as THREE from 'three'
+import { GLView } from 'expo-gl';
+import { Renderer } from 'expo-three';
 
 export default class TreeJsCubeScreen extends React.Component {
-  constructor(props) {
-    super(props)
+  _onGLContextCreate = async (gl) => {
+    const renderer = new Renderer({ gl });
+    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
 
-    this.start = this.start.bind(this)
-    this.stop = this.stop.bind(this)
-    this.animate = this.animate.bind(this)
-  }
-
-  componentDidMount() {
-    const width = this.mount.clientWidth
-    const height = this.mount.clientHeight
-
-    const scene = new THREE.Scene()
+    const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(
-      75,
-      width / height,
-      0.1,
-      1000
-    )
-    const renderer = new THREE.WebGLRenderer({ antialias: true })
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
-    const material = new THREE.MeshBasicMaterial({ color: '#433F81' })
-    const cube = new THREE.Mesh(geometry, material)
+      75, gl.drawingBufferWidth / gl.drawingBufferHeight, 0.1, 1000
+    );
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
 
-    camera.position.z = 4
-    scene.add(cube)
-    renderer.setClearColor('#000000')
-    renderer.setSize(width, height)
+    camera.position.z = 5;
 
-    this.scene = scene
-    this.camera = camera
-    this.renderer = renderer
-    this.material = material
-    this.cube = cube
-
-    this.mount.appendChild(this.renderer.domElement)
-    this.start()
-  }
-
-  componentWillUnmount() {
-    this.stop()
-    this.mount.removeChild(this.renderer.domElement)
-  }
-
-  start() {
-    if (!this.frameId) {
-      this.frameId = requestAnimationFrame(this.animate)
+    const animate = () => {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.07;
+      cube.rotation.y += 0.04;
+      renderer.render(scene, camera);
+      gl.endFrameEXP();
     }
-  }
-
-  stop() {
-    cancelAnimationFrame(this.frameId)
-  }
-
-  animate() {
-    this.cube.rotation.x += 0.01
-    this.cube.rotation.y += 0.01
-
-    this.renderScene()
-    this.frameId = window.requestAnimationFrame(this.animate)
-  }
-
-  renderScene() {
-    this.renderer.render(this.scene, this.camera)
-  }
-
+    animate();
+  };
   render() {
     return (
-      <div
-        style={{ width: '400px', height: '400px' }}
-        ref={(mount) => { this.mount = mount }}
+      <GLView
+        style={{ flex: 1 }}
+        onContextCreate={this._onGLContextCreate}
       />
     )
   }
