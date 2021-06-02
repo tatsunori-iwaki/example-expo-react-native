79277b3417a50f5903391b4091808cf8c7a4918b
tatsunori-iwaki <iwaki.tatsunori@uk-holdings.co.jp>
2021-06-02 19:58:55 +0900

    add screen

---
+++ README.md
@@ -0,0 +1,19 @@
+
+# first setup
+```
+cd ~/IdeaProjects
+expo init example-expo-react-native
+```
+
+# run
+```
+cd ~/IdeaProjects/example-expo-react-native
+yarn start
+```
+
+# modules
+```
+cd ~/IdeaProjects/example-expo-react-native
+expo install react-navigation react-navigation-stack react-native-gesture-handler react-native-reanimated
+```
+
---
+++ screen/HomeScreen.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello, Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
